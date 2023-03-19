189efc86b1d503db16e380bc172f0d58fd782c5a
Volodymyr <vladimir.dvorakovskyi@gmail.com>
2023-03-19 16:02:30 +0200

    Update task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,52 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputEl = document.querySelector('#datetime-picker');
+const btnEl = document.querySelector('[data-start]');
+btnEl.disabled = true;
+let timeDiff = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      window.alert('Please choose a date in the future');
+    } else {
+        btnEl.disabled = false;
+
+      setInterval(() => {
+        timeDiff = selectedDates[0] - new Date();
+      }, 1000);
+    }
+  },
+};
+
+flatpickr(inputEl, options);
+
+const handleBtnClick = () => {
+    console.log(convertMs(timeDiff));
+}
+
+btnEl.addEventListener('click', handleBtnClick);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
